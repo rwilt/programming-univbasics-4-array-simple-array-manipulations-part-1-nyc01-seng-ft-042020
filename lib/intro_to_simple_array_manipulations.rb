3f74700bac98404d08dc7ab813517b4a6078a3ac
rosie w <rosie.wilt@gmail.com>
2020-04-06 22:07:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,19 +19,7 @@ end
 def using_shift(array)
 array.shift
 end
-describe "using_shift" do
-  before(:each) do
-    @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-    @im_so_over_this_city = using_shift(@my_favorite_cities)
-  end
-  it "takes in an argument of an array and uses the shift method to remove the first item and return it" do
-    expect(@im_so_over_this_city).to eq("Lagos")
-  end
 
-  it "decreases the length of the array by 1" do
-    expect(@my_favorite_cities.size).to eq(7)
-  end
-end
 
 describe "shift_with_args" do
   before(:each) do
